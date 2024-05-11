6afd987a595a32c3a5feb4687a68428ed669ecbc
Adnan Erlansyah <adnanerlansyah403@gmail.com>
2024-05-11 17:01:35 +0700

    try with resource - belajar-dasar-oop

---
+++ belajar-java-oop/src/programmer/zaman/now/application/ReadApp.java
@@ -0,0 +1,53 @@
+package programmer.zaman.now.application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadApp {
+
+    public static void main(String[] args) {
+
+//        BufferedReader reader = null;
+
+//        try {
+//            reader = new BufferedReader(
+//                    new FileReader("README.md")
+//            );
+//            while(true) {
+//                String text = reader.readLine();
+//                if(text == null) {
+//                    break;
+//                }
+//                System.out.println(text);
+//            }
+//        } catch (Throwable throwable) {
+//            System.out.println("Error membaca file " + throwable.getMessage());
+//        } finally {
+//            if(reader != null) {
+//                try {
+//                    reader.close();
+//                    System.out.println("Success Menutup");
+//                } catch (IOException exception) {
+//                    System.out.println("Error menutup resource " + exception.getMessage());
+//                }
+//            }
+//        }
+
+        try(BufferedReader reader = new BufferedReader(new FileReader("README.md"))) {
+            while(true) {
+                String text = reader.readLine();
+                if(text == null) {
+                    break;
+                }
+                System.out.println(text);
+            }
+        } catch(Throwable throwable) {
+            System.out.println("Error membaca file " + throwable.getMessage());
+        }
+
+
+
+    }
+
+}
