d31cd8151e9742fdd8f146ba2822c740fdb6d23f
Adnan Erlansyah <adnanerlansyah403@gmail.com>
2024-05-11 11:44:55 +0700

    interface - belajar-dasar-oop

---
+++ belajar-java-oop/src/programmer/zaman/now/data/Avanza.java
@@ -0,0 +1,13 @@
+package programmer.zaman.now.data;
+
+public class Avanza implements Car {
+
+    public void drive() {
+        System.out.println("Driving car avanza");
+    }
+
+    public int getTier() {
+        return 4;
+    }
+
+}
