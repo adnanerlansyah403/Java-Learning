41ed3fefeb0f60959834583128968b42cd385053
Adnan Erlansyah <adnanerlansyah403@gmail.com>
2024-05-11 11:18:26 +0700

    access modifier - belajar-dasar-oop

--- belajar-java-oop/src/programmer/zaman/now/data/Product.java
+++ belajar-java-oop/src/programmer/zaman/now/data/Product.java
@@ -1,14 +1,16 @@
 package programmer.zaman.now.data;
 
 public class Product {
-    String name;
-    int price;
+    public String name;
+    public int price;
+//    protected String name;
+//    protected int price;
+//    private String name;
+//    private int price;
 
-    Product(String name, int price) {
+    public Product(String name, int price) {
         this.name = name;
         this.price= price;
     }
-
-    
 
 }
