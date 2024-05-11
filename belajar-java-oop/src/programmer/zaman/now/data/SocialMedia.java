8716ddb54a8276015b534b6f1736b73ae7cffdfa
Adnan Erlansyah <adnanerlansyah403@gmail.com>
2024-05-11 13:14:52 +0700

    final class - belajar-dasar-oop

---
+++ belajar-java-oop/src/programmer/zaman/now/data/SocialMedia.java
@@ -0,0 +1,13 @@
+package programmer.zaman.now.data;
+
+public class SocialMedia {
+    String name;
+}
+
+final class Facebook extends SocialMedia {
+
+}
+
+//class FakeFacebook extends Facebook {
+//
+//}
