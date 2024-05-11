726cf9b919ef89aa5a7ffee05a9ca46a5c50fdd6
Adnan Erlansyah <adnanerlansyah403@gmail.com>
2024-05-11 11:26:33 +0700

    abstract class - belajar-dasar-oop

---
+++ belajar-java-oop/src/programmer/zaman/now/application/LocationApp.java
@@ -0,0 +1,19 @@
+package programmer.zaman.now.application;
+
+import programmer.zaman.now.data.City;
+import programmer.zaman.now.data.Location;
+
+public class LocationApp {
+
+    public static void main(String[] args) {
+
+        // var location = new Location(); // Error
+
+        var city = new City();
+        city.name = "Bekasi";
+
+        System.out.println(city.name);
+
+    }
+
+}
