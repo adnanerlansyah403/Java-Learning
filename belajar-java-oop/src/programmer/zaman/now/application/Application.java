705dd69107f8517c2e108df34bb6aca2b175987e
Adnan Erlansyah <adnanerlansyah403@gmail.com>
2024-05-11 11:22:43 +0700

    import - belajar-dasar-oop

--- belajar-java-oop/src/programmer/zaman/now/application/Application.java
+++ belajar-java-oop/src/programmer/zaman/now/application/Application.java
@@ -1,6 +1,8 @@
 package programmer.zaman.now.application;
 
-import programmer.zaman.now.data.Product;
+import programmer.zaman.now.data.Product; // Import specific package
+import programmer.zaman.now.data.*; // Import all packages
+// Default import java.lang seperti String, Integer, Boolean, dll
 
 public class Application {
     public static void main(String[] args) {
@@ -8,6 +10,8 @@ public class Application {
         Product product = new Product("Book 2", 50000);
         System.out.println(product.name);
         System.out.println(product.price);
+
+        Data data = new Data();
 
     }
 }
