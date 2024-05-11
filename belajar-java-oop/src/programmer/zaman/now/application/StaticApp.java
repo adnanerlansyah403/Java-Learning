b538379c08c5756a47a85f3928aa115f246ef06e
Adnan Erlansyah <adnanerlansyah403@gmail.com>
2024-05-11 13:50:42 +0700

    static keyword - belajar-dasar-oop

--- belajar-java-oop/src/programmer/zaman/now/application/StaticApp.java
+++ belajar-java-oop/src/programmer/zaman/now/application/StaticApp.java
@@ -2,8 +2,8 @@ package programmer.zaman.now.application;
 
 import programmer.zaman.now.data.Application;
 //import programmer.zaman.now.data.Application.PROCESSORS;
-//import programmer.zaman.now.data.Constant;
-import programmer.zaman.now.data.Constant.*;
+import programmer.zaman.now.data.Constant;
+//import programmer.zaman.now.data.Constant.*;
 import programmer.zaman.now.data.Country;
 import programmer.zaman.now.utils.MathUtil;
 
@@ -11,8 +11,8 @@ public class StaticApp {
 
     public static void main(String[] args) {
 
-        System.out.println(APPLICATION);
-        System.out.println(VERSION);
+        System.out.println(Constant.APPLICATION);
+        System.out.println(Constant.VERSION);
 
         System.out.println(
                 MathUtil.sum(1, 1, 1, 1, 1)
