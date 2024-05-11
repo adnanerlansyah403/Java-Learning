b686f2ed921375801845ef0419f6b2579ef1ceca
Adnan Erlansyah <adnanerlansyah403@gmail.com>
2024-05-12 06:36:50 +0700

    test repository dan service menambah todolist - studi-kasus-todolist-oop

--- studi-kasus-java-dasar-aplikasi-todolist/src/test/service/TodoListServiceTest.java
+++ studi-kasus-java-dasar-aplikasi-todolist/src/test/service/TodoListServiceTest.java
@@ -9,7 +9,7 @@ import service.TodoListServiceImpl;
 public class TodoListServiceTest {
 
     public static void main(String[] args) {
-        testShowTodoList();
+        testAddTodoList();
     }
 
     public static void testShowTodoList() {
@@ -19,6 +19,16 @@ public class TodoListServiceTest {
         todolistRepository.data[2] = new Todolist("Studio Kasus Aplikasi TodoList");
         TodoListService todoListService = new TodoListServiceImpl(todolistRepository);
 
+        todoListService.showTodoList();
+    }
+
+    public static void testAddTodoList() {
+        TodolistRepository todolistRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todolistRepository);
+
+        todoListService.addTodoList("Belajar Java Dasar");
+        todoListService.addTodoList("Belajar Java OOP");
+        todoListService.addTodoList("Studi Kasus Java Aplikasi Todolist");
         todoListService.showTodoList();
     }
 
