63af9540f08f44c887ecd3c4f6f82913d11781c5
Adnan Erlansyah <adnanerlansyah403@gmail.com>
2024-05-16 19:57:36 +0700

    (+) invariant - belajar java generic

---
+++ belajar-java-generic/src/programmer/zaman/now/generic/application/InvariantApp.java
@@ -0,0 +1,24 @@
+package programmer.zaman.now.generic.application;
+
+import programmer.zaman.now.generic.MyData;
+
+public class InvariantApp {
+    public static void main(String[] args) {
+
+        MyData<String> stringMyData = new MyData<>("Adnan");
+//        doIt(stringMyData); // Error
+//        MyData<Object> objectMyData = stringMyData; // Error
+
+        MyData<Object> objectMyData = new MyData<>(1000);
+        MyData<Integer> integerMyData = objectMyData; // Error
+//        doItInteger(objectMyData);
+    }
+
+    public static void doIt(MyData<Object> objectMyData) {
+
+    }
+
+    public static void doItInteger(MyData<Integer> objectMyData) {
+
+    }
+}
